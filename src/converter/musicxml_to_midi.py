511cdc045070cd58d2e5ad4f0b3642a5b63b57be
edvein-rin <edveinrin@gmail.com>
2023-05-08 20:52:33 +0300

    feat: add dummy music XML convert functions

---
+++ src/converter/musicxml_to_midi.py
@@ -0,0 +1,7 @@
+# import music21
+
+def musicxml_to_midi():
+    # c = music21.converter.parse('path/to/music.xml')
+    # c.show('midi')
+
+    raise NotImplementedError
---
+++ src/converter/musicxml_to_sheet_music_pdf.py
@@ -0,0 +1,7 @@
+# import music21
+
+def musicxml_to_sheet_music_pdf():
+    # c = music21.converter.parse('path/to/music.xml')
+    # c.show('sheet-music.pdf')
+
+    raise NotImplementedError
